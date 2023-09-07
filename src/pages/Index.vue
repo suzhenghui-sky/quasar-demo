8dd6b565ce9e5392de943982ba3fb09dca56347c
jyjyjy28 <jyjyjy28@126.com>
2023-09-07 14:23:51 +0800

    one

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      obj: {
+        t: 'ddd',
+      },
+    };
+  },
+
+  mounted() {
+    console.log('djsf', this.obj?.t);
+  },
+};
+</script>
